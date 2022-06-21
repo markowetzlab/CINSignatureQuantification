5eb32bfedd489f73db4f197ca8d1a2a629dba405
Luca Beltrame <luca.beltrame@humanitasresearch.it>
2022-06-21 08:07:15 +0200

    Support build hg38 for the extraction of copy number features
    Two new datasets have been added, `gap_hg38` and `hg38.chrom.sizes`.
    They were made as follows:
    
    - `hg38.chrom.sizes`:
    
    ```
    mysql --user=genome \
        --host=genome-mysql.cse.ucsc.edu \
        -A -e "select chrom, size from hg38.chromInfo" > hg38.chrom sizes
    ```
    
    Alternative contigs were then removed manually and the data loaded in R,
    and saved as `hg38.chrom.sizes.rda`.
    
    - `hg38_gap`:
    
    ```
    mysql --user=genome \
        --host=genome-mysql.cse.ucsc.edu \
        -A -e "select * from hg38.gap" > gap_hg38
    ```
    
    The data was then loaded directly in R and saved as `gap_hg38.rda`.
    
    To allow alternative genome builds, an additional `build` parameter was
    added to the functions, defaulting to `hg19` for compatibility reason.
    
    The code tries to infer this from the `ExpData`'s `build` slot.
    
    My limited testing has showed this code to be "working" in principle but
    I'd like more eyes over it.

--- R/startCopynumberFeatureExtractionDrews.R
+++ R/startCopynumberFeatureExtractionDrews.R
@@ -1,11 +1,11 @@
-startCopynumberFeatureExtractionDrews = function(dtSmooth, cores = 1, RMNORM = TRUE) {
+startCopynumberFeatureExtractionDrews = function(dtSmooth, cores = 1, RMNORM = TRUE, build="hg19") {
 
     # Convert to data frame
     dfBR = data.frame(dtSmooth)
     # Split by sample
     lBR = split( dfBR, dfBR$sample )
     # Extract features
-    brECNF = extractCopynumberFeaturesDrews(lBR, cores = cores, rmNorm = RMNORM)
+    brECNF = extractCopynumberFeaturesDrews(lBR, cores = cores, rmNorm = RMNORM, build=build)
 
     return(brECNF)
 }
