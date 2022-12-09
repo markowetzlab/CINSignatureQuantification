1f65e4729eb4c4845c50722283d807860f9b4886
Philip Smith <philsmith990@hotmail.co.uk>
2022-12-09 22:39:16 +0000

    test and coverage plotActivites
    Added unit testing for plotActivites to meet reasonable code coverage

--- tests/testthat/test-plotActivities.R
+++ tests/testthat/test-plotActivities.R
@@ -1,10 +1,60 @@
 ## plotActivities
 # signature activities
 
+data("SIGobj.drews")
+data("SIGobj.mac")
+
 test_that("null input", {
   expect_error(plotActivities())
 })
 
 test_that("class input error", {
     expect_error(plotActivities("data"))
+})
+
+test_that("unknown method", {
+    expect_error(plotActivities(object = SIGobj.drews,type = "unknown"))
+})
+
+test_that("drews method threshold", {
+    expect_type(plotActivities(object = SIGobj.drews,type = "threshold"),"list")
+    expect_no_condition(plotActivities(object = SIGobj.drews,type = "threshold"))
+})
+
+test_that("mac method threshold", {
+    expect_type(plotActivities(object = SIGobj.mac,type = "threshold"),"list")
+    expect_no_condition(plotActivities(object = SIGobj.mac,type = "threshold"))
+})
+
+test_that("drews method raw", {
+    expect_type(plotActivities(object = SIGobj.drews,type = "raw"),"list")
+    expect_no_condition(plotActivities(object = SIGobj.drews,type = "raw"))
+})
+
+test_that("mac method raw", {
+    expect_type(plotActivities(object = SIGobj.mac,type = "raw"),"list")
+    expect_no_condition(plotActivities(object = SIGobj.mac,type = "raw"))
+})
+
+test_that("drews method norm", {
+    expect_type(plotActivities(object = SIGobj.drews,type = "norm"),"list")
+    expect_no_condition(plotActivities(object = SIGobj.drews,type = "norm"))
+})
+
+test_that("mac method norm", {
+    expect_error(plotActivities(object = SIGobj.mac,type = "norm"))
+})
+
+test_that("drews method scaled", {
+    expect_type(plotActivities(object = SIGobj.drews,type = "scaled"),"list")
+    expect_no_condition(plotActivities(object = SIGobj.drews,type = "scaled"))
+})
+
+test_that("mac method scaled", {
+    expect_error(plotActivities(object = SIGobj.mac,type = "scaled"))
+})
+
+
+test_that("custom colours", {
+    expect_error(plotActivities(object = SIGobj.drews,cols = c("1","2")))
 })
