897e44d7e75d66112dc4a54bc3a6dc462b4161ed
Philip Smith <philsmith990@hotmail.co.uk>
2024-09-13 17:07:31 +0100

    Added labelled events
    Added osCN event labelling function and comparison of terminating and non-terminating count distributions

--- cnsignatures_osCN_refit/osCN_source_funcs.R
+++ cnsignatures_osCN_refit/osCN_source_funcs.R
@@ -180,6 +180,66 @@ getOscillationDrewsV2 = function(abs_profiles, chrlen) {
     return(data.frame(out,stringsAsFactors = FALSE))
 }
 
+## pre-package V2 functions with labelling of chain location
+getOscillationDrewsV2WITHLABEL = function(abs_profiles, chrlen) {
+
+    # Prepare looping
+    out = c()
+    samps = names(abs_profiles)
+    # Loop over samples
+    for(i in samps) {
+
+        # Retrieve segments
+        segTab = abs_profiles[[i]]
+        colnames(segTab)[4] = "segVal"
+
+        # Loop over chromosomes to identify oscillation
+        chrs = unique(segTab$chromosome)
+        oscCounts = c()
+        osLabel = c()
+        for(c in chrs) {
+
+            currseg = as.numeric(segTab$segVal[segTab$chromosome == c])
+            currseg = round(as.numeric(currseg))
+
+            # Only take chains into consideration with a length of more than 3 elements
+            if(length(currseg)>3) {
+                prevval = currseg[1]
+                count = 0
+                for(j in 3:length(currseg)) {
+                    if(currseg[j] == prevval & currseg[j] != currseg[j-1]) {
+                        count = count+1
+                        # suggested fix for missing end of chromosome chain counts
+                        # https://github.com/markowetzlab/CINSignatureQuantification/issues/22
+                        if (j == length(currseg)) {
+                            oscCounts = c(oscCounts, count)
+                            osLabel = c(osLabel, TRUE)
+                            count = 0
+                        }
+                    } else {
+                        oscCounts = c(oscCounts,count)
+                        osLabel = c(osLabel, FALSE)
+                        count = 0
+                    }
+                    prevval = currseg[j-1]
+                }
+            }
+        }
+        # Make sure it's really numeric
+        out = rbind(out, cbind(ID = rep(i,length(oscCounts)),
+                               value = as.numeric(oscCounts),
+                                label = as.logical(osLabel)))
+        if(length(oscCounts) == 0) {
+            out = rbind(out,cbind(ID = i, value = 0,label = FALSE))
+        }
+    }
+
+    # Prepare return
+    rownames(out) = NULL
+    return(data.frame(out,stringsAsFactors = FALSE))
+}
+
+
 calculateSampleByComponentMatrixDrewsV2 = function(brECNF, UNINFPRIOR = TRUE,weights=Drews2022_v2_TCGA_Mixture_Models) {
 
     # Load mix models
